f0ba76b948f19f6d254780a2ca8d2e162cfac9d9
Josefina Gonzalez Cornet <117315719+jgonzalezcornet@users.noreply.github.com>
2023-11-13 12:37:44 -0300

    fix tpe.sh

--- tpe.sh
+++ tpe.sh
@@ -5,13 +5,9 @@ SEASON_URL="https://api.sportradar.us/rugby-league/trial/v3/en/seasons.xml?api_k
 #Get seasons.xml file from API
 curl -X GET $SEASON_URL
 
-#Ask the user for season_year
-echo "Enter the season year"
-read year
-
-#Ask the user for season_prefix
-echo "Enter the season prefix"
-read prefix
+#Reassign parameter values to local variables
+prefix = $1
+year = $2
 
 #Get the season_id (via extract_season_id.xq) and assign it to local variable "id"
 $id=(java net.sf.saxon.Query extract_season_id.xq java net.sf.saxon.Query extract_season_id.xq -ext season_year=$year season_prefix=$prefix)
