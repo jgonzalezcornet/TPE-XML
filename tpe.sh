fcfd8d99f973592c137c782171ee4c19e380ed2a
Josefina Gonzalez Cornet <117315719+jgonzalezcornet@users.noreply.github.com>
2023-11-13 12:48:51 -0300

    update tpe.sh

--- tpe.sh
+++ tpe.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 API_KEY="xrbr4d7jguscwkdg7hu393gt"
-SEASON_URL="https://api.sportradar.us/rugby-league/trial/v3/en/seasons.xml?api_key={API_KEY}"
 
 #Get seasons.xml file from API
-curl -X GET $SEASON_URL
+curl -X GET "https://api.sportradar.us/rugby-league/trial/v3/en/seasons.xml?api_key=${API_KEY}" -o seasons.xml
 
 #Reassign parameter values to local variables
 prefix = $1
@@ -13,10 +12,10 @@ year = $2
 $id=(java net.sf.saxon.Query extract_season_id.xq java net.sf.saxon.Query extract_season_id.xq -ext season_year=$year season_prefix=$prefix)
 
 #Get season_info.xml
-$ curl -X GET "https://api.sportradar.us/rugby-league/trial/v3/en/seasons/$id/info.xml?api_key={$API_KEY}" > season_info.xml
+$ curl -X GET "https://api.sportradar.us/rugby-league/trial/v3/en/seasons/$id/info.xml?api_key=${API_KEY}" -o season_info.xml
 
 #Get season_lineups.xml
-$ curl -X GET "https://api.sportradar.us/rugby-league/trial/v3/en/seasons/$id/lineups.xml?api_key={$API_KEY}" > season_lineups.xml
+$ curl -X GET "https://api.sportradar.us/rugby-league/trial/v3/en/seasons/$id/lineups.xml?api_key=${API_KEY}" -o season_lineups.xml
 
 #Generate season_data.xml (via extract_season_data.xq)
 java net.sf.saxon.Query extract_season_data.xq > season_data.xml
