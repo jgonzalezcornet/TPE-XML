775192418200f924f4c743b5c57f6713e314835d
Josefina Gonzalez Cornet <117315719+jgonzalezcornet@users.noreply.github.com>
2023-11-13 13:25:33 -0300

    fix tpe.sh

--- tpe.sh
+++ tpe.sh
@@ -8,6 +8,9 @@ curl -X GET "https://api.sportradar.us/rugby-league/trial/v3/en/seasons.xml?api_
 prefix = $1
 year = $2
 
+#Remove the XML Schema Namespace from seasons.xml
+!!!
+
 #Get the season_id (via extract_season_id.xq) and assign it to local variable "id"
 id=$(java net.sf.saxon.Query extract_season_id.xq java net.sf.saxon.Query extract_season_id.xq -ext season_year=$year season_prefix=$prefix \!method=text)
 
@@ -16,6 +19,9 @@ $ curl -X GET "https://api.sportradar.us/rugby-league/trial/v3/en/seasons/$id/in
 
 #Get season_lineups.xml
 $ curl -X GET "https://api.sportradar.us/rugby-league/trial/v3/en/seasons/$id/lineups.xml?api_key=${API_KEY}" -o season_lineups.xml
+
+#Remove the XML Schema NameSpace from season_info.xml and season_lineups.xml
+!!!
 
 #Generate season_data.xml (via extract_season_data.xq)
 java net.sf.saxon.Query extract_season_data.xq > season_data.xml
